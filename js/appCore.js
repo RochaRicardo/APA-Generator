7c0890df08bd8ceca9441f9a76fbf6cfccee7c7f
RochaRicardo <ricardo@MacBookPro.local>
2017-06-27 08:18:56 +0100

    add event handlers

--- js/appCore.js
+++ js/appCore.js
@@ -1,15 +1,3 @@
-document.getElementById("bookOpt").onclick = function () {
-    this.classList.add("clicked");
-};
-document.getElementById("magazineOpt").onclick = function () {
-    this.classList.add("clicked");
-};
-document.getElementById("aboutOpt").onclick = function () {
-    this.classList.add("clicked");
-};
 
-document.getElementById("buttonToGenerate").onclick = function () {
-    //TODO: call the method to generate the APA reference
-};
 
 
